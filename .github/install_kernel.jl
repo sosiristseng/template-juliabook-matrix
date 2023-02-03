84a00d007f8d4484a2a7531b872fe321eed65a40
Wen-Wei Tseng <sosiristseng@gmail.com>
2023-02-03 11:57:57 +0800

    cleaner info

--- .github/install_kernel.jl
+++ .github/install_kernel.jl
@@ -5,5 +5,5 @@ using IJulia
 p = installkernel("Julia", "--project=@.")
 newpath = joinpath(dirname(p), "julia")
 
-@info "Creating symlink " newpath " pointing to" p
+@info "Creating symlink: $newpath"
 symlink(p, newpath, dir_target=true)
