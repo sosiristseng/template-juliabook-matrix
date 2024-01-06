5d024d9802e1f902a924d1ff3fc733606ffc2935
Wen Wei Tseng <sosiristseng@gmail.com>
2024-01-07 00:06:14 +0800

    no docker

--- .github/execute.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-KERNEL_NAME=$(julia -e 'print("julia-1.",VERSION.minor)')
-
-julia -e 'import IJulia; IJulia.installkernel("Julia", "--project=@.")'
-
-jupyter nbconvert --to notebook --execute --inplace ${EXTRA_ARGS} \
-    --ExecutePreprocessor.timeout=${TIMEOUT} \
-    --ExecutePreprocessor.kernel_name=${KERNEL_NAME} \
-    "docs/${NOTEBOOK}"
