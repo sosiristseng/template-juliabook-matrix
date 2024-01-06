6dc12742619b5e6b40da7499f210a21edc3bae38
Wen Wei Tseng <sosiristseng@gmail.com>
2024-01-06 23:01:49 +0800

    CI

--- .github/execute.sh
+++ .github/execute.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
+KERNEL_NAME=$(julia -e 'print("julia-1.",VERSION.minor)')
 
 julia -e 'import IJulia; IJulia.installkernel("Julia", "--project=@.")'
 
 jupyter nbconvert --to notebook --execute --inplace ${EXTRA_ARGS} \
     --ExecutePreprocessor.timeout=${TIMEOUT} \
-    --ExecutePreprocessor.kernel_name=$(julia -e 'print("julia-1.",VERSION.minor)') \
+    --ExecutePreprocessor.kernel_name=${KERNEL_NAME} \
     "docs/${NOTEBOOK}"
