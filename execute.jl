d254af4a38e538bd2cebf43639022e8dc3e21016
Wen Wei Tseng <sosiristseng@gmail.com>
2024-03-19 21:29:52 +0800

    fix

--- execute.jl
+++ execute.jl
@@ -12,7 +12,7 @@ else
     error("$(file) is not a valid notebook file!")
 end
 
-outpath = joinpath(get(ENV, "GITHUB_WORKSPACE", pwd()), cachedir, nb)
+outpath = joinpath(get(ENV, "WORKSPACE", pwd()), cachedir, nb)
 mkpath(dirname(outpath))
 
 kernelname = "--ExecutePreprocessor.kernel_name=julia-1.$(VERSION.minor)"
