3e71f4452307b7c13de6720cd015bd2784ac5136
Wen Wei Tseng <sosiristseng@gmail.com>
2024-03-19 21:33:41 +0800

    fix

--- execute.jl
+++ execute.jl
@@ -16,9 +16,10 @@ outpath = joinpath(get(ENV, "WORKSPACE", pwd()), cachedir, nb)
 mkpath(dirname(outpath))
 
 kernelname = "--ExecutePreprocessor.kernel_name=julia-1.$(VERSION.minor)"
-extraargs = get(ENV, "EXTRA_ARGS", "")
+extraargs = get(ENV, "EXTRA_ARGS", " ")
+
 timeout = "--ExecutePreprocessor.timeout=" * get(ENV, "TIMEOUT", "-1")
 output = "--output $(outpath)"
-cmd = `jupyter nbconvert --to notebook --execute $(extraargs) $(timeout) $(kernelname) $(nb)`
+cmd = `jupyter nbconvert --to notebook --execute $(extraargs) $(timeout) $(kernelname) $(output) $(nb)`
 
 run(cmd)
