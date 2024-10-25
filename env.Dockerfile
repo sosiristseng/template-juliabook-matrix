89c8a6faf2857609cb7cdcea6cdbf004af09e573
Wen Wei Tseng <sosiristseng@gmail.com>
2024-10-25 15:14:20 +0800

    offline mode

--- env.Dockerfile
+++ env.Dockerfile
@@ -1,5 +1,6 @@
 FROM julia:1.11.1
 
+# Julia settings
 ENV JULIA_CI='true'
 ENV JULIA_NUM_THREADS='auto'
 ENV JULIA_CONDAPKG_ENV="/opt/conda"
--- instantiate.jl
+++ instantiate.jl
@@ -6,6 +6,7 @@ CondaPkg.add(["jupyter", "nbconvert", "matplotlib"])
 ENV["JUPYTER"] = CondaPkg.which("jupyter")
 Pkg.add(["IJulia", "Literate", "JSON"])
 
+ENV["JULIA_CONDAPKG_OFFLINE"] = true
 Pkg.activate(".")
 Pkg.instantiate()
 Pkg.precompile()
